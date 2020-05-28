e252d819c9e676cb6b21e9e2f0a1fa87db542caf
Jerry Wang <jerry@JerrydeMBP.hitronhub.home>
2020-05-28 10:59:30 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="number">Number</div>
+      <div className="btn-gp">
+        <button>-</button>
+        <button>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
